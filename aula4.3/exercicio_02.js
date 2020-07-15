db0022a545c984aa5fd5c3fe89dbc3eefc389540
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-15 15:22:48 -0300

    aula 4-3

---
+++ aula4.3/exercicio_02.js
@@ -0,0 +1,9 @@
+//Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. 
+
+let asteristicos = 5;
+let linha = "";
+
+for (let index = 0; index <= asteristicos; index +=1){
+    linha = linha + "*";
+    console.log(linha);
+}
---
+++ aula4.3/exercicio_03.js
@@ -0,0 +1,9 @@
+//Agora inverta o lado do triângulo. exemplo exercicio 2
+
+let asteristicos = 5;
+let linha = "";
+
+for (let index = 0; index <= asteristicos; index +=1){
+    linha = linha + "*";
+    console.log(linha);
+}

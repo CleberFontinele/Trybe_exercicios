3bfe2d4b2e02d34664bdb0a0a1d4e473034018fc
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-16 10:54:47 -0300

    rename

---
+++ aula4.3/exercicio06Bonus.js
@@ -0,0 +1,19 @@
+// Faça um programa que diz se um número definido numa variável é primo ou não.
+// Um número primo é um número que só é divisível por 1 e por ele mesmo, ou seja, 
+// a divisão dele com quaisquer outros números dá resto diferente de zero.
+// Dica: você vai precisar de fazer um loop que vá de 0 ao número definido; Além disso, 
+// vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+let n = 5;
+let primo = 0;
+
+for (let cont = 1; cont <= n; cont +=1) {
+    if (n % cont === 0) {
+        primo += 1;
+    }
+}
+if (primo > 2) {
+    console.log("Não é primo!");
+} else {
+    console.log("È numero primo!")
+}

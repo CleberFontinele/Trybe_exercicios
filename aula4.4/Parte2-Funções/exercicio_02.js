637f1d804087f8eccddcf3a7e9aa5c8cd28f810a
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-16 23:32:19 -0300

    aula 4.4

---
+++ aula4.4/Parte2-Funções/exercicio_02.js
@@ -0,0 +1,13 @@
+//Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+//Array de teste: [2, 3, 6, 7, 10, 1];.
+//Valor esperado no retorno da função: 4.
+
+let n = [2, 3, 6, 7, 10, 1];
+function maiorValor(n){
+    for (let index = 0; index < array.length; index++) {
+        let maior = array[index];
+        return maior;
+}
+console.log(n)
+}
+

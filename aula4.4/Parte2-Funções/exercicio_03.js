e178942022686ffcf818b2328ccd0f25eda72c63
CleberFontinele <cleberfontinele2387@gmail.com>
2020-07-21 23:38:59 -0300

    ex3-aula 4.4

--- aula4.4/Parte2-Funções/exercicio_03.js
+++ aula4.4/Parte2-Funções/exercicio_03.js
@@ -2,3 +2,13 @@
 // Array de teste: [2, 4, 6, 7, 10, 0, -3];.
 // Valor esperado no retorno da função: 6.
 
+function menorValor(array){
+let menor = 0;
+for (let index = 0; index < array.length; index+= 1) {
+    if (array[menor] > array[index]){
+     menor = index;
+    }
+}
+return menor 
+}
+console.log(menorValor([2, 4, 6, 7, 10, 0, -3]));

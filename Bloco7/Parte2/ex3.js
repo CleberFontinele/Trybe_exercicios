3fbab01cc0c05da89f32fe367464ba636f240ccb
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-10 19:29:07 -0300

    ex.3 incompleto

---
+++ Bloco7/Parte2/ex3.js
@@ -0,0 +1,8 @@
+let clickCount = 0;
+let textToDisplay = document.getElementById("text");
+
+document.getElementById("button_test")
+.addEventListener("click", () => textToDisplay.innerHTML = clickCount += 1);
+
+
+console.log("oi")

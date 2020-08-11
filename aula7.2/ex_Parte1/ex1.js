e4c0e21169f7df6cdd4f1ff67c116f20ce665153
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-11 18:41:06 -0300

    atualizando propriedades do objeto

---
+++ aula7.2/ex_Parte1/ex1.js
@@ -0,0 +1,19 @@
+// playerNumber deveria ser um número
+// A variável playerdeve ser uma string
+// O valor de playerdeve ser "Montana"
+// Você deve usar a notação de colchetes para acessar testObj
+// Você não deve atribuir o valor Montanaà variável playerdiretamente.
+// Você deve usar a variável playerNumberem sua notação de colchetes
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+  };
+  
+  // Only change code below this line
+  
+  var playerNumber=16;       // Change this line
+  var player = testObj[playerNumber];   // Change this line
+console.log(testObj);  

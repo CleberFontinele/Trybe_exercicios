03f055c59c6d9b27e2c11885aec67c27e5994473
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-11 19:17:13 -0300

    atualizando propriedades do objeto

---
+++ aula7.2/ex_Parte1/ex2.js
@@ -0,0 +1,13 @@
+// Você deve atualizar myDoga "name"propriedade de para ser igual a "Happy Coder".
+// Você não deve editar a myDogdefinição.
+
+// Setup
+var myDog = {
+    "name": "Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+  };
+  myDog.name = 'Happy Coder'
+  // Only change code below this line
+console.log(myDog.name);  

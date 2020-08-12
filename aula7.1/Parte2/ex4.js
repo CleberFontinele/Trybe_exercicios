55ef10120ef5fc754267c7ba02e3513c1fe6aed8
CleberFontinele <cleberfontinele2387@gmail.com>
2020-08-12 08:59:05 -0300

    usando template literais

--- aula7.1/Parte2/ex4.js
+++ aula7.1/Parte2/ex4.js
@@ -1,25 +1,23 @@
 const skills = ['javaScript', 'css', 'html'];
 const substituteWordInArray = string => {
-    let stringPassed = "Testando a impressão do x"
+    let stringPassed = "Dev x loading..."
     let word = stringPassed.replace('x', string)
     return word;
 }
-console.log(skills)
 
-const juntos = text => {
+const string = text => {
     var newOrd = 0;
-    var ordenado = softskills.sort()
+    var ordeString = skills.sort()
     let newString = `
     ${text} 
     Minhas 5 habilidades são:
     `
     
-    for (var i = 0; i < ordenado.length; i += 1) {
-    newOrd = ordenado[i]
-    newString += `
-    ${newOrd}
+    for (let i = 0; i < ordeString.length; i += 1) {
+    newOrd = ordeString[i]
+    newString += `${newOrd}
     `
     }
     return newString
     }
-    console.log(juntos(substX('bru'))) 
+    console.log(string(substituteWordInArray('Cleber'))) 
